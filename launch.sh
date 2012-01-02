216efd175930409b25271215dd2ac5de8b2e2064
nemo <nemo@nemo.(none)>
2012-01-02 18:42:09 +0100

    Add launcher with amount argument

---
+++ launch.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+PIDS=""
+
+
+cleanup() {
+	for i in $PIDS; do
+		echo "Killing $i"
+		kill $i
+	done
+}
+
+trap cleanup 1 2 3 6
+
+for i in `seq 1 $1`; do
+	(./tdm -station=$i) &
+
+	if [ -z "$PIDS" ]; then
+		PIDS="$!"
+	else
+		PIDS="$PIDS $!"
+	fi
+done
+
+for i in $PIDS; do
+	echo "Waiting for $i"
+	wait $i
+done
+
+
